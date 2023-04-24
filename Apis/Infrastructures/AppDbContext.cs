f32e3a0a2cbd4e76b64b3e1aec56fd71349263ed
Vo Thuong Truong Nhon <vothuongtruongnhon2002@gmail.com>
2023-04-24 20:39:11 +0700

    Initial commit

---
+++ Apis/Infrastructures/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructures
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Chemical> Chemicals { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
