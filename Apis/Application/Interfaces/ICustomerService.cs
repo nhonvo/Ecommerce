65bc058889054a539fcdc6f61cab8b13dfaf50f8
Vo Thuong Truong Nhon <vothuongtruongnhon2002@gmail.com>
2023-05-01 20:09:56 +0700

    add delete customer order

--- Apis/Application/Interfaces/ICustomerService.cs
+++ Apis/Application/Interfaces/ICustomerService.cs
@@ -14,13 +14,14 @@ namespace Application.Interfaces
     public interface ICustomerService
     {
         Task<ApiResult<Pagination<CustomerResponse>>> GetAsync(int pageIndex, int pageSize);
-        Task<ApiResult<Pagination<CustomerResponse>>> GetOrder(Guid id, int pageIndex, int pageSize);
-        Task<ApiResult<OrderResponse>> AddOrder(Guid id);
-        Task<ApiResult<OrderResponse>> UpdateOrder(Guid id, Guid orderId, UpdateCustomerOrder request);
+        Task<ApiResult<CustomerResponse>> GetAsync(Guid Id);
         Task<ApiResult<CustomerResponse>> AddAsync(CreateCustomer request);
         Task<ApiResult<CustomerResponse>> Update(UpdateCustomer request);
         Task<ApiResult<CustomerResponse>> Delete(string Id);
-        Task<ApiResult<CustomerResponse>> Get(Guid Id);
+        Task<ApiResult<Pagination<CustomerResponse>>> GetOrder(Guid id, int pageIndex, int pageSize);
+        Task<ApiResult<OrderResponse>> AddOrder(Guid id);
+        Task<ApiResult<OrderResponse>> UpdateOrder(Guid id, Guid orderId, UpdateCustomerOrder request);
+        Task<ApiResult<OrderResponse>> DeleteOrder(Guid id, Guid orderId);
 
     }
 }
